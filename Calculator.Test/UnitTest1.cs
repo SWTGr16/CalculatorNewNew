f4d4390bba821608211f7c437d292b65898b7d09
Danny Bøjstrup Pedersen <20031954@post.au.dk>
2021-02-09 14:28:56 +0100

    Ændring i testprojekt. Har oprettet instans af uut i Setup.

--- Calculator.Test/UnitTest1.cs
+++ Calculator.Test/UnitTest1.cs
@@ -5,37 +5,35 @@ namespace Calculator.Test
 {
     public class Tests
     {
+        public Calc uut;
+
         [SetUp]
         public void Setup()
         {
+            uut = new Calc();
         }
 
         [Test]
         public void Add2and4Returns6()
         {
-            var uut = new Calc();
-
             Assert.That(uut.Add(2, 4), Is.EqualTo(6));
         }
 
         [Test]
         public void Subtract4and2Returns2()
         {
-            var uut = new Calc();
             Assert.That(uut.Subtract(4, 2), Is.EqualTo(2));
         }
 
         [Test]
         public void Multiply2and3Returns6()
         {
-            var uut = new Calc();
             Assert.That(uut.Multiply(2, 3), Is.EqualTo(6));
         }
 
         [Test]
         public void Power4and2Returns16()
         {
-            var uut = new Calc();
             Assert.That(uut.Power(4, 2), Is.EqualTo(16));
         }
     }
