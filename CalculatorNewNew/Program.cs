1a818ca9d88523f5135eecf8ebc74ad535e5e36a
KerstinFroberg <au639158@post.au.dk>
2021-02-10 09:41:22 +0100

    Random ændring - test

--- CalculatorNewNew/Program.cs
+++ CalculatorNewNew/Program.cs
@@ -6,7 +6,7 @@ namespace CalculatorNewNew
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello random!");
         }
     }
 }
